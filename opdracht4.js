c4bc990b624487272b03be14fb03afe2a6dd2304
Merijn <99054535@mydavinci.nl>
2019-10-30 21:22:27 +0100

    opdracht4 voltooid
    voltooid

--- opdracht4.js
+++ opdracht4.js
@@ -1,6 +1,34 @@
-var number = prompt("welke tafel wilt u?")
-    for(var i = 1; i < number; i++){
-        for(j = 0; j <= 10; j++){
-            
+
+ 
+
+while(true) {
+        var tafel = prompt("welke tafel wilt u?")
+
+        if ( isNaN(tafel) == false ) {
+            //het is een getal
+
+            if (tafel <= 10){
+                //mooi tabel maken
+                for (var i = 1; i <= tafel; i++){
+                    for(j = 1; j <= 10; j++){
+                        document.write(i+"x"+ j +"="+ i*j +"<br>")
+                    
+                    }
+                    document.write("<br> <br>")
+                }
+                break;
+            }
+          else if(tafel >= 10){
+                alert("graag een getal onder de 10")
+                // het getal is te groot
+                // stop
+          }
+        }
+        else {
+            alert("graag een getal invoeren")//het is geen getal
+            // stop
+
         }
     }
+
+
