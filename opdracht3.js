4cee44a5c6ad49ded0a8a1ce7653bc341e3f80cc
Merijn <99054535@mydavinci.nl>
2019-10-30 10:13:14 +0100

    Bezig met opdracht3
    update

--- opdracht3.js
+++ opdracht3.js
@@ -1,3 +1,13 @@
-function opdracht(){
-    
+var name = prompt("Wat is uw naam?")
+var age = prompt("Wat is uw leeftijd?")
+function opdracht(name, age){
+    for(i = 1; i < stop; i++){
+    if (name + age){
+        document.write("Hallo " + name + ", je leeftijd is "+ age)
+    }
+        else if(name == "stop" || age == "stop"){
+         break;
+        }
+    }
 }
+opdracht(name, age)
