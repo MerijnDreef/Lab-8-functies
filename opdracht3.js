bdbbe556d2ae0f6a57b3d32687c58988682f4e9f
Merijn <99054535@mydavinci.nl>
2019-10-30 15:18:54 +0100

    opdracht3 voltooid
    voltooid

--- opdracht3.js
+++ opdracht3.js
@@ -1,13 +1,9 @@
-var name = prompt("Wat is uw naam?")
-var age = prompt("Wat is uw leeftijd?")
-function opdracht(name, age){
-    for(i = 1; i < stop; i++){
-    if (name + age){
-        document.write("Hallo " + name + ", je leeftijd is "+ age)
-    }
-        else if(name == "stop" || age == "stop"){
-         break;
+do{
+    naam = prompt("Wat is uw naam?")
+        if(naam != "stop"){
+            leeftijd = prompt("Wat is uw leeftijd?")
+            if(leeftijd != "stop"){
+                document.write("Hallo"+ naam +", je leeftijd is"+ leeftijd +"<br>")
+            }
         }
-    }
-}
-opdracht(name, age)
+    }while(naam != "stop"&& leeftijd != "stop")
---
+++ opdracht4.js
@@ -0,0 +1,7 @@
+var number = prompt("welke tafel wilt u?")
+    if(number <= 10){
+
+    }
+    if(number >= 10){
+        alert("graag een getal onder de 10")
+    }
